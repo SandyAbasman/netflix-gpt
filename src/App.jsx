7256223ac97c81ddae05d72851a3a4ea6f9a36dc
Sandy Abasman <abasifrekesandy@gmail.com>
2025-07-13 20:28:15 +0200

    starting

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <p className="text-3xl font-bold
+      
+      
+      
+      
+      
+      
+      text-red-300">app</p>
+    </div>
+  );
+}
+
+export default App;
